ed5fdf0d613140a57028fe5809f5a95010ff5f0a
Benjamin Katz <bakatz91@gmail.com>
2017-11-17 01:02:02 -0800

    Adds About page, updates test data (fixes #2)

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,16 +1,17 @@
 <template>
-  <span>
-      {{ msg }}
-  </span>
+  <div class="jumbotron">
+    <h1 class="display-3">About Us</h1>
+    <hr class="my-4">
+    <p>CrashTested was built by a small team of Software Engineers working out of San Diego, California. 
+      Click <a href="mailto:hello@crashtested.co">here</a> to contact us.</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'About',
-  data () {
-    return {
-      msg: 'hey man about'
-    }
+  mounted () {
+    this.$Progress.finish()
   }
 }
 </script>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,7 +5,8 @@
     <hr class="my-4">
     <p>We built CrashTested because safety standards for motorcycle gear are confusing and often meaningless. Instead of scouring the internet, looking for clues as to which standards your gear is held to, just click below to get accurate and up-to-date information about your favorite motorcycle gear!</p>
     <p class="lead">
-      <router-link to="/products" class="btn btn-primary btn-lg" role="button">Learn more</router-link>
+      <router-link to="/products" class="btn btn-primary btn-lg" role="button">View Products</router-link>
+      <router-link to="/about" class="btn btn-primary btn-lg" role="button">Learn more</router-link>
     </p>
   </div>
 </template>
