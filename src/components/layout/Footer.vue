9055efe4cfa2060e799454f501a0d6ac11870cec
Benjamin Katz <bakatz91@gmail.com>
2017-11-25 15:35:18 -0800

    Fixes footer spacing issues

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,8 +1,6 @@
 <template>
     <footer class="footer app-footer">
-        <div class="container">
-            <span class="text-muted">© 2017 CrashTested. Portions of the product data © SHARP under the <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government License v3.0</a>, except where otherwise stated.</span>
-        </div>
+      <span class="text-muted">© 2017 CrashTested. Portions of the product data © SHARP under the <a href="http://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/">Open Government License v3.0</a>, except where otherwise stated.</span>
     </footer>
 </template>
 
@@ -26,7 +24,9 @@ html {
     bottom: 0;
     width: 100%;
     height: 60px; /* Set the fixed height of the footer here */
-    line-height: 60px; /* Vertically center the text there */
     background-color: #f5f5f5;
+    position: absolute;
+    text-align: center;
+    padding-top: 20px;
 }
 </style>
