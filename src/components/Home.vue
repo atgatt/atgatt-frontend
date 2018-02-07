bbdc81773fbadc9c274096f48ecca9dd197424cf
Benjamin Katz <bakatz91@gmail.com>
2018-02-06 21:39:17 -0800

    Updates marketing speak

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,7 +3,7 @@
     <h1 class="display-3">Motorcycle crashes are either sad stories or epic tales.</h1>
     <p class="lead">Which would you like to star in?</p>
     <hr class="my-4">
-    <p>If you're the "epic tale" kind of person, you're in the right place. We built CrashTested to be the most comprehensive, safe, and trusted source of motorcycle safety data on the internet.</p>
+    <p>If you're the "epic tale" kind of person, you're in the right place. We built CrashTested to be the most comprehensive and trusted source of motorcycle safety data on the internet.</p>
     <p class="lead">
       <router-link to="/ratings" class="btn btn-primary btn-lg" role="button">View Ratings</router-link>
       <router-link to="/about" class="btn btn-secondary btn-lg" role="button">About Us</router-link>
