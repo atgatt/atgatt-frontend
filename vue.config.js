227cdb48bdb2cc9e48ec5f611718e2f869af8f01
Benjamin Katz <bakatz91@gmail.com>
2018-11-10 13:49:00 -0800

    Changes proxy config to point to staging instead of localhost

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api-proxy': {
-        target: 'http://localhost:5000',
+        target: 'https://api-staging.crashtested.co',
         changeOrigin: true,
         pathRewrite: {
           '^/api-proxy': ''
