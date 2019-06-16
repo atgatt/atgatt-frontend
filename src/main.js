b1ab2480100a9843ed0c6d0e8a1e970cef9fb375
Karthik Kumar <karthik@lightstep.com>
2019-06-16 14:22:17 -0700

    rename file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import AuthMiddleware from './lib/authPlugin'
+import AuthMiddleware from './lib/authMiddleware'
 
 import VueProgressBar from 'vue-progressbar'
 
