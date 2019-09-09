2355aa151fb1747fde2b76971246452d0aa0e78b
Benjamin Katz <bakatz91@gmail.com>
2019-09-08 22:50:22 -0700

    Fixes navigation ordering

--- src/components/common/layout/NavBar.vue
+++ src/components/common/layout/NavBar.vue
@@ -14,8 +14,8 @@
         <nav-item route="/about-helmet-ratings" text="About Helmets" />
         <nav-item route="/about-jacket-ratings" text="About Jackets" />
         <nav-item route="/about-pants-ratings" text="About Pants" />
-        <nav-item route="/about-gloves-ratings" text="About Gloves" />
         <nav-item route="/about-boots-ratings" text="About Boots" />
+        <nav-item route="/about-gloves-ratings" text="About Gloves" />
       </ul>
       <ul class="navbar-nav ml-auto">
         <nav-item route="/about-us" text="About Us" />
